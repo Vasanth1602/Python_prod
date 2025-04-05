0168c39288e7036686d50d982bcee439d222a054
Vasanth1602 <kumarajith85666@gmail.com>
2025-04-05 12:57:10 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
 
 app = Flask(__name__)
 
+# Create a simple Counter metric
+REQUEST_COUNTER = Counter('http_requests_total', 'Total HTTP requests received')
+
 @app.route('/')
 def home():
-    return "Hello,Guys!"
+    REQUEST_COUNTER.inc()  # Increment the counter on each request
+    return "Hello, Prometheus!"
+
+# Prometheus metrics endpoint
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
